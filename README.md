4df3dff1e74845c3d814a5eb7d8fe4f0d7d7b166
Dannywei0914 <a0932819576@gmail.com>
2020-06-19 13:42:36 +0800

    add scroll

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 靜態介面作品製作-有想到什麼就加
+
 目前放了css動畫
+
 js部分用了setInterval
 
-$ node initProject.js
+> $ node initProject.js
 
-$ npm start
+> $ npm start
--- js/show.js
+++ js/show.js
@@ -1,12 +1,24 @@
-import $ from './jquery-3.5.1.min';
+import jquery from "jquery";
+
+window.$ = window.jQuery = jquery;
+
+
 
 $(document).ready(function() {
   var img1 = $('#img1');
   var img2 = $('#img2');
   var icon = $('#icon');
+  console.log($('.body'))
+
 
   window.setInterval(() => {
     img1.toggleClass('sliders-option_show');
     img2.toggleClass('sliders-option_show');
   }, 5000);
+
+  $('#icon').click(function () {
+    window.scrollTo({
+      top: $('.body').get(0).offsetTop,
+      behavior: 'smooth',
+    })  })
 })
